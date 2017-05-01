aeeb1f2f9fb53dc60c6615b541f3f5e7a3abff36
KenOokamiHoro <horo@yoitsu.moe>
2017-05-01 19:42:05 +0800

    update irclogs

--- app/irclog/views.py
+++ app/irclog/views.py
@@ -8,17 +8,20 @@ import os,datetime,json,glob,random
 
 @irclog.route("/")
 def index():
-    channels = { j:["Today"] for j in sorted([i.replace("/home/horo/.weechat/logs/freenode/","").replace(".weechatlog","") for i in glob.glob("/home/horo/.weechat/logs/freenode/*.weechatlog")]) if "#" in j }
+    channels = { j:["Today"] for j in sorted([i.replace("/home/horo/.weechat/logs/freenode/","") for i in glob.glob("/home/horo/.weechat/logs/freenode/*")]) if "#" in j }
     for channel in channels:
-        channels[channel].extend(sorted([i.replace("/home/horo/.weechat/logs/freenode_","") for i in glob.glob("/home/horo/.weechat/logs/freenode_*")]))
-
+        channels[channel].extend(sorted([i.replace("/home/horo/.weechat/logs/freenode/{}/".format(channel),"").replace(".log","") for i in glob.glob("/home/horo/.weechat/logs/freenode/{}/*".format(channel))]))
+        try:
+            channels[channel].remove(str(datetime.date.today()))
+        except ValueError:
+            channels[channel].remove('Today')
     return render_template("irclog/index.html",channels=channels)
 
 @irclog.route('/<channel>',methods=["GET","POST"])
 def view(channel):
     channel=channel.replace("$","#")
     try:
-        log_files=[ open(i) for i in glob.glob("/home/horo/.weechat/logs/freenode*/#{}.weechatlog".format(channel))]
+        log_files=[ open(i) for i in glob.glob("/home/horo/.weechat/logs/freenode/{}/*".format(channel))]
         if not log_files:
             raise FileNotFoundError
     except FileNotFoundError:
@@ -34,9 +37,9 @@ def view_day(channel,day):
     channel=channel.replace("$","#")
     try:
         if day.lower() == "today":
-            log_file = open("/home/horo/.weechat/logs/freenode/#{}.weechatlog".format(channel))
+            log_file = open("/home/horo/.weechat/logs/freenode/{}/{}.log".format(channel,str(datetime.date.today())))
         else:
-            log_file = open("/home/horo/.weechat/logs/freenode_{}/#{}.weechatlog".format(day,channel))
+            log_file = open("/home/horo/.weechat/logs/freenode/{}/{}.log".format(channel,day))
         logs = [j for j in [i.rstrip().split("\t") for i in log_file.readlines()] if j[1] != "--"]
         for log in logs:
             log[0] = log[0][10:]
